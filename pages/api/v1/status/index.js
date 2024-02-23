e6a6483de214129695dfec460ac791059b3a9d3f
Bruno Costa <ihyperbr@gmail.com>
2024-02-23 16:02:04 +0000

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -9,23 +9,20 @@ async function status(request, response) {
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query(
-    " SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = '';"
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = '';'"
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = ''; SELECT pg_sleep(4); ';"
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = ''; SELECT pg_sleep(4); --';"
+
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
-
-  // const resultQueryVersionDataBase = await database.query(
-  //   "SELECT split_part(version(), ' ', 2) as version",
-  // );
-
-  // const resultMaxConnectionsDataBase = await database.query(
-  //   "SHOW max_connections;",
-  // );
-
-  // const resultCountConnectionsDataBase = await database.query(
-  //   "SELECT count(*) as count_connections FROM pg_stat_activity;",
-  // );
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -36,15 +33,6 @@ async function status(request, response) {
         opened_connections: databaseOpenedConnectionsValue,
       },
     },
-    // database: {
-    //   postgres_version: parseFloat(resultQueryVersionDataBase.rows[0].version),
-    //   max_connections: new Number(
-    //     resultMaxConnectionsDataBase.rows[0].max_connections,
-    //   ),
-    //   count_connections: new Number(
-    //     resultCountConnectionsDataBase.rows[0].count_connections,
-    //   ),
-    // },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,20 +10,16 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-
-  // expect(responseBody).toHaveProperty("database");
-
-  // const database = responseBody.database;
-
-  // expect(database).toHaveProperty("postgres_version");
-  // expect(typeof database.postgres_version).toBe("number");
-
-  // expect(database).toHaveProperty("max_connections");
-  // expect(typeof database.max_connections).toBe("number");
-
-  // expect(database).toHaveProperty("count_connections");
-  // expect(typeof database.count_connections).toBe("number");
-  // expect(database.count_connections).toBeLessThanOrEqual(
-  //   database.max_connections,
-  // );
 });
+
+// test.only("SQL INJECTION TEST", async () => {
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=");
+//   await fetch("http://localhost:3000/api/v1/status?databaseName=';");
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4);",
+//   );
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+//   );
+// });
